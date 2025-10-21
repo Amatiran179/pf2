66cbea18a03641e28036a6c7b111ea7f45a93471
Amatiran179 <angga26442@gmail.com>
2025-10-21 10:27:27 +0700

    feat(cta): inline/floating/modal + exit-intent + tracking stub

--- functions.php
+++ functions.php
@@ -15,3 +15,4 @@ require_once get_template_directory() . '/inc/core/autoload.php';
 require_once get_template_directory() . '/inc/core/setup.php';
 require_once get_template_directory() . '/inc/core/enqueue.php';
 require_once get_template_directory() . '/inc/core/hooks.php';
+require_once get_template_directory() . '/inc/helpers/cta.php';
